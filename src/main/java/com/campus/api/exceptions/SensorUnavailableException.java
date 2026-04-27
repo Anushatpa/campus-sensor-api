60be616e4be529ef273a4218bf9a8633a2935377
Anushatpa <anusha.tpa@gmail.com>
2026-04-27 15:42:04 +0545

    SensorUnavailableException.java

---
+++ src/main/java/com/campus/api/exceptions/SensorUnavailableException.java
@@ -0,0 +1,10 @@
+package com.campus.api.exceptions;
+
+/**
+ * Mapped to HTTP 403 Forbidden by SensorUnavailableExceptionMapper.
+ */
+public class SensorUnavailableException extends RuntimeException {
+    public SensorUnavailableException(String message) {
+        super(message);
+    }
+}
