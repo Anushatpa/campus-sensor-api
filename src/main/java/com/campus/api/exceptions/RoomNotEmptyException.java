0922b49c40ce58ab291cfd9aa3f93ad24670df10
Anushatpa <anusha.tpa@gmail.com>
2026-04-27 10:47:08 +0545

    RoomNotEmptyException.java

---
+++ src/main/java/com/campus/api/exceptions/RoomNotEmptyException.java
@@ -0,0 +1,10 @@
+package com.campus.api.exceptions;
+
+/*
+ * Mapped to HTTP 409 Conflict by RoomNotEmptyExceptionMapper.
+ */
+public class RoomNotEmptyException extends RuntimeException {
+    public RoomNotEmptyException(String message) {
+        super(message);
+    }
+}
