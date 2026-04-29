0f6106ee1f1d15cd4e64f4b51eee45d55c4a2460
Anushatpa <anusha.tpa@gmail.com>
2026-04-29 08:07:20 +0545

    ApiLoggingFilter.java

---
+++ src/main/java/com/campus/api/filters/ApiLoggingFilter.java
@@ -0,0 +1,31 @@
+package com.campus.api.filters;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class ApiLoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger LOGGER = Logger.getLogger(ApiLoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        String method = requestContext.getMethod();
+        String uri    = requestContext.getUriInfo().getRequestUri().toString();
+        LOGGER.info(String.format("[REQUEST]  --> %s %s", method, uri));
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        int    status = responseContext.getStatus();
+        String method = requestContext.getMethod();
+        String uri    = requestContext.getUriInfo().getRequestUri().toString();
+        LOGGER.info(String.format("[RESPONSE] <-- %d  (%s %s)", status, method, uri));
+    }
+}
